b755cb19dffc1c3746016dcd513224004eeb9c02
SigureMo <sigure.qaq@gmail.com>
2020-05-18 23:25:50 +0800

    :docs: chore: add submodules

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,31 +4,68 @@ module.exports = {
   base: "/vuepress-lovely-plugins/",
 
   plugins: [
+    // 彩带背景
     [
       "ribbon",
       {
-        size: 90,     // width of the ribbon, default: 90
-        opacity: 0.8, // opacity of the ribbon, default: 0.3
-        zIndex: 0     // z-index property of the background, default: -1
-      }
+        size: 90,
+        opacity: 0.8,
+        zIndex: -1,
+      },
     ],
+    // 鼠标特效插件
     [
-      'cursor-effects'
+      "cursor-effects",
+      {
+        size: 1.75,
+        shape: "star",
+      },
     ],
+    // 离开页面标题变化
     [
       "dynamic-title",
       {
         showIcon: "/favicon.ico",
-        showText: "(/≧▽≦/)咦！又好了！",
+        showText: "(ฅ>ω<*ฅ)欢迎回来！",
         hideIcon: "/failure.ico",
-        hideText: "(●—●)喔哟，崩溃啦！",
-        recoverTime: 2000
-      }
+        hideText: "( ๑ˊ•̥▵•)੭₎₎不要走呀！",
+        recoverTime: 2000,
+      },
     ],
-    ["go-top"]
+    // 悬挂小猫返回顶部
+    ["go-top"],
+    // Meting 插件
+    [
+      "meting",
+      {
+        meting: {
+          server: "netease",
+          type: "playlist",
+          mid: "2539599584",
+        },
+        aplayer: {
+          lrcType: 3,
+        },
+      },
+    ],
   ],
 
   themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Packages", link: "/packages" }
+    ],
+    sidebarDepth: 1,
+    sidebar: {
+      "/": [
+        "",
+        "packages/ribbon",
+        "packages/go-top",
+        "packages/dynamic-title",
+        "packages/cursor-effects",
+        "packages/meting"
+      ]
+    },
     repo: "SigureMo/vuepress-lovely-plugins",
     docsDir: "docs",
     docsBranch: "master"
---
+++ scripts/collect-docs.sh
@@ -0,0 +1,6 @@
+mkdir docs/packages/
+cp packages/vuepress-plugin-ribbon/README.md docs/packages/ribbon.md
+cp packages/vuepress-plugin-go-top/README.md docs/packages/go-top.md
+cp packages/vuepress-plugin-cursor-effects/README.md docs/packages/cursor-effects.md
+cp packages/vuepress-plugin-dynamic-title/README.md docs/packages/dynamic-title.md
+cp packages/vuepress-plugin-meting/README.md docs/packages/meting.md
