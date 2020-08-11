b9e8f5bc75662fe80a54b5c85e7a50adb3c3ea08
SigureMo <sigure.qaq@gmail.com>
2020-08-12 01:28:18 +0800

    📝 docs: optimize docs structure

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,72 +1,83 @@
+const path = require('path')
+const packages = [
+  'ribbon',
+  'cursor-effects',
+  'dynamic-title',
+  'go-top',
+  'meting'
+]
+
 module.exports = {
-  title: "moefy-vuepress",
-  description: "Add some lovely plugins in your vuepress!",
-  base: "/moefy-vuepress/",
+  title: 'moefy-vuepress',
+  description: 'Add some lovely plugins in your vuepress!',
+  base: '/moefy-vuepress/',
 
   plugins: [
     // 彩带背景
     [
-      "ribbon",
+      'ribbon',
       {
         size: 90,
         opacity: 0.8,
-        zIndex: -1,
-      },
+        zIndex: -1
+      }
     ],
     // 鼠标特效插件
     [
-      "cursor-effects",
+      'cursor-effects',
       {
         size: 1.75,
-        shape: "star",
-      },
+        shape: 'star'
+      }
     ],
     // 离开页面标题变化
     [
-      "dynamic-title",
+      'dynamic-title',
       {
-        showIcon: "/favicon.ico",
-        showText: "(ฅ>ω<*ฅ)欢迎回来！",
-        hideIcon: "/failure.ico",
-        hideText: "( ๑ˊ•̥▵•)੭₎₎不要走呀！",
-        recoverTime: 2000,
-      },
+        showIcon: '/favicon.ico',
+        showText: '(ฅ>ω<*ฅ)欢迎回来！',
+        hideIcon: '/failure.ico',
+        hideText: '( ๑ˊ•̥▵•)੭₎₎不要走呀！',
+        recoverTime: 2000
+      }
     ],
     // 悬挂小猫返回顶部
-    ["go-top"],
+    ['go-top'],
     // Meting 插件
     [
-      "meting",
+      'meting',
       {
         meting: {
-          server: "netease",
-          type: "playlist",
-          mid: "2539599584",
+          server: 'netease',
+          type: 'playlist',
+          mid: '2539599584'
         },
         aplayer: {
-          lrcType: 3,
-        },
-      },
-    ],
+          lrcType: 3
+        }
+      }
+    ]
   ],
 
   themeConfig: {
-    nav: [
-      { text: "Home", link: "/" }
-    ],
+    nav: [{ text: 'Home', link: '/' }],
     sidebarDepth: 1,
     sidebar: {
-      "/": [
-        "",
-        "packages/ribbon",
-        "packages/go-top",
-        "packages/dynamic-title",
-        "packages/cursor-effects",
-        "packages/meting"
-      ]
+      '/': packages.map(package => `packages/${package}`)
     },
-    repo: "moefyit/moefy-vuepress",
-    docsDir: "docs",
-    docsBranch: "master"
-  }
-};
+    repo: 'moefyit/moefy-vuepress',
+    docsDir: 'docs',
+    docsBranch: 'master'
+  },
+
+  // 作为插件的额外路由
+  additionalPages: packages.map(package => {
+    return {
+      path: `/packages/${package}.html`,
+      filePath: path.resolve(
+        __dirname,
+        `../../packages/vuepress-plugin-${package}/README.md`
+      )
+    }
+  })
+}
--- scripts/collect-docs.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p docs/packages/
-cp -f packages/vuepress-plugin-ribbon/README.md docs/packages/ribbon.md
-cp -f packages/vuepress-plugin-go-top/README.md docs/packages/go-top.md
-cp -f packages/vuepress-plugin-cursor-effects/README.md docs/packages/cursor-effects.md
-cp -f packages/vuepress-plugin-dynamic-title/README.md docs/packages/dynamic-title.md
-cp -f packages/vuepress-plugin-meting/README.md docs/packages/meting.md
